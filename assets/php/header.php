d9892783020e150580249cce04f9903a449bf597
Aristea Lachana <alachana@csd.auth.gr>
2022-05-13 12:09:11 +0300

    seperate header footer

---
+++ assets/php/header.php
@@ -0,0 +1,33 @@
+<div class="upperBackground">
+  <div class="logo" >
+      <a href="mainPage.html"> <img alt="Improve Your Health" src="logo.png"
+         width="400" height="100"></a>
+  </div>
+<br><br>
+<div class="dropdown">
+<button onclick="myFunction()" class="dropbtn">Ο λογαριασμός μου</button>
+<div id="myDropdown" class="dropdown-content">
+  <a href="#">Link 1</a>
+  <a href="#">Link 2</a>
+  <a href="#">Link 3</a>
+</div>
+</div>
+  <form class="search" style="margin:auto;max-width:400px">
+    <input type="text"  placeholder="Search.." name="search">
+    <button type="submit"> <i class="fa fa-search"></i></button>
+  </form>
+</div>
+
+  <div class="page">
+    <nav class="page__menu page__custom-settings menu">
+
+      <ul class="menu__list r-list">
+        <li class="menu__group"><a href="mainPage.html" class="menu__link r-link text-underlined">ΑΡΧΙΚΗ ΣΕΛΙΔΑ</a></li>
+        <li class="menu__group"><a href="categories.html" class="menu__link r-link text-underlined">ΠΕΡΙΕΧΟΜΕΝΟ</a></li>
+        <li class="menu__group"><a href="eidikoi.html" class="menu__link r-link text-underlined">Η ΟΜΑΔΑ ΜΑΣ</a></li>
+        <li class="menu__group"><a href="contactpage.html" class="menu__link r-link text-underlined">ΕΠΙΚΟΙΝΩΝΙΑ</a></li>
+
+      </ul>
+
+    </nav>
+  </div>
