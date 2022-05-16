0699ebca22e14c12a646be08ffcdc0f5d695a196
AristeidisMatskidis <arismats@csd.auth.gr>
2022-05-17 01:20:26 +0300

    Screen sizing header v1

--- header.php
+++ header.php
@@ -1,7 +1,30 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <meta charset = "UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="pspi.css">
+  <!-- Font icons pulled from remix icon CDN (Content delivery network) -->
+ <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+ <!-- Link to your stylesheet -->
+ <link rel="stylesheet" href="./styles.css">
+ <!-- -->
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Exercise</title>
+  <link rel="icon" type="image/x-icon" href="favicon.png">
+
+  <style>
+
+  body {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  /*a{text-decoration: none;}*/
+  </style>
+</head>
 <div class="upperBackground">
-  <div class="logo" >
+  <div class="logo logoSmall" >
       <a href="mainPage.php"> <img alt="Improve Your Health" src="logo.png"
          width="400" height="100"></a>
   </div>
