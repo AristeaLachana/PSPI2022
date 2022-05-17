3cdd0beb32f06898743aeb7c27ac1bda1e018c32
AIKATERINI-KONSTANTINA SAITI <saikaterin@csd.auth.gr>
2022-05-17 20:14:04 +0300

    javascript signup

--- signup.php
+++ signup.php
@@ -43,24 +43,24 @@
       <tr>
           <td>
            <form class="name" action="/sendData" method="post" style="margin:auto;max-width:300px">
-             <input type="text" name="username" minlength="5" maxlength="15" placeholder="Username">
+             <input type="text" name="username" minlength="5" maxlength="15" placeholder="Username" required>
            </form>
           </td>
           <td>
             <form class="email" action="/sendData" method="post"  style="margin:auto;max-width:300px">
-               <input type="email" name="email" placeholder="Email">
+               <input type="email" name="email" placeholder="Email" required>
              </form>
           </td>
        </tr>
        <tr>
           <td>
             <form class="firstname" action="/sendData" method="post" style="margin:auto;max-width:300px">
-              <input type="text" name="firstname" minlength="3" maxlength="30" placeholder="First name">
+              <input type="text" name="firstname" minlength="3" maxlength="30" placeholder="First name" required>
             </form>
           </td>
           <td>
            <form class="surname" action="/sendData" method="post" style="margin:auto;max-width:300px">
-            <input type="text" name="surname" minlength="3" maxlength="30" placeholder="Surname">
+            <input type="text" name="surname" minlength="3" maxlength="30" placeholder="Surname" required>
            </form>
           </td>
        </tr>
@@ -70,7 +70,7 @@
           </th>
           <td>
            <form class="date" action="/sendData" method="post" style="margin:auto;max-width:300px">
-            <input type="date" id="date">
+            <input type="date" id="date" required>
            </form>
           </td>
        </tr>
@@ -80,7 +80,7 @@
          </th>
          <td>
            <form class="password" action="/sendData" method="post" style="margin:auto;max-width:300px">
-             <input type="password" id="password" name="password" minlength="8" maxlength="16">
+             <input type="password" id="password" name="password" minlength="8" maxlength="16"pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
            </form>
          </td>
        </tr>
@@ -90,7 +90,7 @@
          </th>
          <td>
            <form class="passwordverification" action="/sendData" method="post" style="margin:auto;max-width:300px">
-             <input type="password" id="passwordverification" name="password" minlength="8" maxlength="16">
+             <input type="password" id="passwordverification" name="password" minlength="8" maxlength="16" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
            </form>
          </td>
        </tr>
@@ -108,6 +108,7 @@
          <td>
           <form class="expertcategories" action="/sendData" method="post" style="margin:auto;max-width:300px">
             <select id="categories" name="expertcategories">
+              <option value="null" selected>  </option>
               <option value="nutritionist" selected>ΔΙΑΤΡΟΦΟΛΟΓΙΑ</option>
               <option value="fitness specialist" >ΓΥΜΝΑΣΤΙΚΗ</option>
               <option value="mental health specialist" >ΨΥΧΙΚΗ ΥΓΕΙΑ</option>
@@ -141,6 +142,20 @@
        </tr>
     </table>
   </div>
+  <div id="message">
+  <h3>Password must contain the following:</h3>
+  <p id="letter" class="invalid">Ένα <b>μικρό λατινικό</b> γράμμα</p>
+  <p id="capital" class="invalid">Ένα <b>κεφαλαίο λατινικό</b> γράμμα</p>
+  <p id="number" class="invalid">Έναν <b>αριθμό</b></p>
+  <p id="length" class="invalid">Τουλάχιστον <b>8 χαρακτήρες</b></p>
+  </div>
+  <div id="message1">
+  <h3>Password must contain the following:</h3>
+  <p id="letter1" class="invalid1">Ένα <b>μικρό λατινικό</b> γράμμα</p>
+  <p id="capital1" class="invalid1">Ένα <b>κεφαλαίο λατινικό</b> γράμμα</p>
+  <p id="number1" class="invalid1">Έναν <b>αριθμό</b></p>
+  <p id="length1" class="invalid1">Τουλάχιστον <b>8 χαρακτήρες</b></p>
+  </div>
 
 </div>
 <br>
@@ -150,5 +165,155 @@
 <?php
 include 'includes\footer2.php';
  ?>
+ <script>
+
+function myFunction1() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("passwordverification");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+var myInput = document.getElementById("passwordverification");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+var myInput1 = document.getElementById("password");
+var letter1 = document.getElementById("letter1");
+var capital1 = document.getElementById("capital1");
+var number1 = document.getElementById("number1");
+var length1 = document.getElementById("length1");
+
+// When the user clicks on the password field, show the message box
+myInput1.onfocus = function() {
+  document.getElementById("message1").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput1.onblur = function() {
+  document.getElementById("message1").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput1.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters1 = /[a-z]/g;
+  if(myInput1.value.match(lowerCaseLetters1)) {
+    letter1.classList.remove("invalid1");
+    letter1.classList.add("valid1");
+  } else {
+    letter1.classList.remove("valid1");
+    letter1.classList.add("invalid1");
+  }
+
+  // Validate capital letters
+  var upperCaseLetters1 = /[A-Z]/g;
+  if(myInput1.value.match(upperCaseLetters1)) {
+    capital1.classList.remove("invalid1");
+    capital1.classList.add("valid1");
+  } else {
+    capital1.classList.remove("valid1");
+    capital1.classList.add("invalid1");
+  }
+
+  // Validate numbers
+  var numbers1 = /[0-9]/g;
+  if(myInput1.value.match(numbers1)) {
+    number1.classList.remove("invalid1");
+    number1.classList.add("valid1");
+  } else {
+    number1.classList.remove("valid1");
+    number1.classList.add("invalid1");
+  }
+
+  // Validate length
+  if(myInput1.value.length >= 8) {
+    length1.classList.remove("invalid1");
+    length1.classList.add("valid1");
+  } else {
+    length1.classList.remove("valid1");
+    length1.classList.add("invalid1");
+  }
+}
+function Validate() {
+       var password = document.getElementById("password").value;
+       var confirmPassword = document.getElementById("passwordverification").value;
+       if (password != confirmPassword) {
+           alert("Passwords do not match.");
+           return false;
+       }
+       return true;
+}
+
+
+</script>
+
+
+
+
 </body>
 </html>
