a959d7f469fce5b6ae75249d4bf128189ca01bff
Aristea Lachana <alachana@csd.auth.gr>
2022-05-27 19:56:18 +0300

    add page success page

--- addpagesuccess.php
+++ addpagesuccess.php
@@ -1,6 +1,6 @@
 <?php include('mustLogin.php')?>
 <?php require_once('config.php')?>
-<?php include('send_message.php')?>
+<?php include('upload.php')?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -15,67 +15,48 @@
    <!-- -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-
-    <title>Improve Your Health | Επικοινωνία</title>
+    <title>Improve Your Health | Προσθήκη Αρχείου</title>
     <link rel="icon" type="image/x-icon" href="uploads\favicon.png">
     <style>
       img {
         display: block;
         margin-left: auto;
-        margin-right:  auto;
+        margin-right: auto;
       }
-      body {background-color:white;}
-      h1 {color:#125688;}
-      h2 {color:black;}
-      h5 {color:#ED6964;}
+      h5 {color:black;}
       p {color:black;}
-
-
      body {
       margin: 0;
+
       font-family: Arial, Helvetica, sans-serif;
      }
-
-
     </style>
   </head>
 
   <body>
-
     <?php
-      include 'includes\header.php';
+    include 'includes\header.php';
      ?>
+
   <br>
-    <div class="center">
-            <h2><strong>ΕΠΙΚΟΙΝΩΝΙΑ</strong></h2>
-    </div>
+      <div class="center">
+        <h2><strong>ΠΡΟΣΘΗΚΗ ΠΕΡΙΕΧΟΜΕΝΟΥ</strong></h2>
+     </div>
+     <div class="add-style">
 
+    <div class="containerSuccess">
+       <br>
+      <p style="color:black; size:30px">Το άρθρο σας καταχωρήθηκε! Αναμένεται έγκριση από τον διαχειριστή.</p>
+       <br>
+       <button class="btn" type="submit" name="new_post_btn"><a href="addpage.php" style="color:white;text-decoration: none;">Προσθήκη Νέου Άρθρου</a></button>
 
- <div class="bg-img1">
+     </div>
 
-   <form action="" method="post" class="containercontact" style="text-align:center; position:relative; margin: auto; float: center;">
-
-     <br>
-
-     <p style="color:white; size:30px">Το μήνυμά σας στάλθηκε επιτυχώς! Θα επικοινωνήσουμε μαζί σας άμεσα.</p>
-
-     <br>
-
-     <button class="btn" type="submit" name="send_new_message_btn"><a href="contactpage.php" style="color:white;text-decoration: none;">Αποστολή Νέου Μηνύματος</a></button>
-
-    </form>
-
-    <br>
-    <br>
-
-</div>
-</body>
-<br>
-</br>
+  </br>
+  </div>
 <?php
 include 'includes\footer.php';
  ?>
-
 <script>
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -89,7 +70,6 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 
 function showSlides(n) {
   let i;
@@ -107,3 +87,4 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 </script>
+</html>
